9f25272dd22d770fa592a46b894ad1395af9ac65
Aleksey Kondratev <pendalph@hotmail.com>
2023-02-09 12:53:58 +0300

    basic configure project

---
+++ cmd/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/pendalph/lumen/internal/app/server"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/server.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+	config := server.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := server.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/pendalph/lumen
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/sirupsen/logrus v1.9.0
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
